4568248f77ccacd93cb9842bc41ac80d3356331f
Hugo Bottois <bottois.hugo@outlook.fr>
2021-07-17 22:59:45 +0200

    seed + experiences

--- seed.py
+++ seed.py
@@ -19,19 +19,44 @@ from django.contrib.auth.models import User
 
 
 Profile.objects.all().delete()
+Experience.objects.all().delete()
 
+
+profile_test = "Im a Life scientist passionate about the intersections of Healthcare and Data science and programming. During my thesis, I performed statistical and data visualization analysis on clinical and biological data (transcriptomic, flow cytometry) resulting in several scientific publications and oral presentations in international conferences. I also worked on machine learning projects applied to medical images and radiomics for classification and segmentation tasks. I have experience in back-end development (Python, SQL, Flask, Django) and front-end interface (HTML, CSS, JS).Im strongly motivated to work on projects aiming to have an impact on healthcare and clinical practice."
 
 def seed():
   hugo =  User.objects.get(username="hbottois")
 
-  profile = Profile.objects.create(
+  Profile.objects.create(
     first_name = "Hugo",
     last_name="Bottois",
     title =  "PhD",
     profile_pic = "https://res.cloudinary.com/dmeefs6iu/image/upload/v1626531093/public/images/avatar2_g5nxgg.jpg",
-    about_me = "ee szkcncùklj vcoẑejdnvc ojz voẑev ozrenvonzvoikn ",
+    about_me = profile_test,
     user = hugo
   )
+  Experience.objects.create(
+  job_name = "Project Learder",
+  company = "Capgemini Enginners",
+  logo_company = "https://res.cloudinary.com/dmeefs6iu/image/upload/v1626531093/public/images/avatar2_g5nxgg.jpg",
+  description =  profile_test,
+  start_date = '2019-09-02',
+  end_date = '2021-09-02',
+  User = hugo
+  )
+
+  Experience.objects.create(
+  job_name = "Project Learder",
+  company = "Capgemini Enginners",
+  logo_company = "https://res.cloudinary.com/dmeefs6iu/image/upload/v1626531093/public/images/avatar2_g5nxgg.jpg",
+  description =  profile_test,
+  start_date = '2019-09-02',
+  end_date = '2021-09-02',
+  User = hugo
+  )
+
+
+
   print("done")
 
 
