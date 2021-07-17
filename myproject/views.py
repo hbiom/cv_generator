710c2c57169d45c191acbc238db046340bee6b8e
Hugo Bottois <bottois.hugo@outlook.fr>
2021-07-17 15:59:04 +0200

    profile section

--- myproject/views.py
+++ myproject/views.py
@@ -2,13 +2,13 @@ from django.shortcuts import render
 from mycv.models import *
 
 def home(request):
-  profile = Profile.objects.all()
+  profiles = Profile.objects.all()
   experiences = Experience.objects.all()
   skills = Skill.objects.all()
   publications = Publication.objects.all()
   studies = Study.objects.all()
 
-  return render(request, 'home.html',{'experiences':experiences})
+  return render(request, 'home.html',{'experiences':experiences,"profiles":profiles})
 
 
 
