80d4cd7f68bec592307f2a45f33e922cac5ea72c
Hugo Bottois <bottois.hugo@outlook.fr>
2021-08-04 19:00:18 +0200

    setting heroku db

--- myproject/settings.py
+++ myproject/settings.py
@@ -14,6 +14,7 @@ import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 # security key
 import dotenv
