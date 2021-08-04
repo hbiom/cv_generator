317596b1d93a7692ab9adfca6a54d773b726c98b
Hugo Bottois <bottois.hugo@outlook.fr>
2021-08-04 02:16:54 +0200

    spotme tags

--- myproject/templatetags/custom_tags.py
+++ myproject/templatetags/custom_tags.py
@@ -4,14 +4,14 @@ from django import template
 register = template.Library()
 
 
-
+#  to optimse
 def spotme(author, me):
   '''
   me is a single name
   author is a string containing names including me
   This function return author string with <span></span> around me
   '''
-  return author.replace(me, f"<span>{me}</span>")
+  return author.replace(me, f"<span class=\"me\">{me}</span>")
 
 
 register.filter("spotme",spotme)
@@ -19,6 +19,9 @@ register.filter("spotme",spotme)
 import re
 
 def networkIcon(icon_name):
+  '''
+  return dictionary containing font awesome logo for social network
+  '''
 
   NETWORK_icon = {
     "linkedin":"fab fa-linkedin-square",
